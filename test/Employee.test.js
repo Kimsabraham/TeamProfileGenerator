d82e98df29687629b7c26526cdca21b08ae872c8
Abraham Kim <krnpride730@yahoo.com>
2021-04-23 11:41:01 -0400

    test with name constructor

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,6 +1,12 @@
 const Employee = require("../lib/employee");
 
-test("Employee instance works", () => {
+test("Can instantiate employee instance", () => {
   const e = new Employee();
   expect(typeof e).toBe("object");
+});
+
+test("name passes with constructor", () => {
+  const name = "Bob";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
 });
