c1b3adedf1d2215b3c0fa68e0ab01f4b79e9b679
Abraham Kim <krnpride730@yahoo.com>
2021-04-23 17:07:59 -0400

    arguments should pass value extending from employee and user input

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -22,3 +22,27 @@ test("email passes with constructor", () => {
   const e = new Employee("Bob", 1, pass);
   expect(e.email).toBe(pass);
 });
+
+test("getName argument passes()", () => {
+  const pass = "Bob";
+  const e = new Employee(pass);
+  expect(e.getName()).toBe(pass);
+});
+
+test("getId arguemnt passes()", () => {
+  const pass = true;
+  const e = new Employee("Bob", pass);
+  expect(e.getId()).toBe(pass);
+});
+
+test("getEmail argument passes()", () => {
+  const pass = "fakegmail@gmail.com";
+  const e = new Employee("Bob", 1, pass);
+  expect(e.getEmail()).toBe(pass);
+});
+
+test('getRole() returns "Employee"', () => {
+  const pass = "Employee";
+  const e = new Employee("Bob", 1, "fakeemail@gmail.com");
+  expect(e.getRole()).toBe(pass);
+});
